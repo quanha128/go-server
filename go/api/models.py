4abbb02da085d3ecfa6fd62f3f04bb8ead6cecac
Quan Ha <s1f101910027@iniad.org>
2020-12-22 14:08:40 +0900

    modify models.py

--- go/api/models.py
+++ go/api/models.py
@@ -1,23 +1,32 @@
 from django.db import models
-from datetime import datetime
-from django.db.models.base import Model
-from django.db.models.deletion import CASCADE
-from django.utils import timezone
-from django.contrib.auth.models import User as AuthUser
-from django import forms
-from django.contrib.auth.forms import UserCreationForm as Form
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Player(models.Model):
+    game_id = models.ForeignKey('Game', on_delete = models.SET_NULL, blank = True, null = True)
+    is_playing = models.BooleanField(null= False, default = False)
+    is_spectating = models.BooleanField(null= False, default = False)
+    chat_id = models.ForeignKey('Chatline', on_delete = models.SET_NULL, blank = True, null = True)
+    avatar_url = models.CharField(max_length=255, default="")
+    elo = models.IntegerField(null=False, default = 1000)
 
 class Game(models.Model):
-    id = models.CharField(default="", primary_key=True, max_length=6)
-    board_state = """array"""
-    time_start = models.DateTimeField()
-    time_end = models.DateTimeField()
+    player1_id = models.ForeignKey(Player, related_name="P1", on_delete = models.SET_NULL, blank = True, null = True)
+    player2_id = models.ForeignKey(Player, related_name="P2", on_delete = models.SET_NULL, blank = True, null = True)
+    board_state = ArrayField(
+        ArrayField(
+            models.IntegerField(null=False, default=0),
+            size=19,
+        ),
+        size=19,
+    )
+    #player_turn = models.IntergerField(null=False, defalut = 0)
+    time_start = models.DateTimeField(auto_now_add = True)
+    time_end = models.DateTimeField(auto_now_add = True)
 
-class Player(AuthUser):
-    is_playing = models.BooleanField(default=False)
-    is_spectating = models.BooleanField(default=False)
-    avatar_url = models.CharField(default="", max_length=500)
-    elo = models.IntegerField(default=0)
-    game_id = models.ForeignKey(Game, on_delete=CASCADE)
+class Chatline(models.Model):
+    game_id = models.ForeignKey(Game, on_delete = models.CASCADE)
+    chat_id = models.CharField(max_length=255, default="")
+    sayer = models.ForeignKey(Player, on_delete = models.SET_NULL, blank = True, null = True)
+    line = models.CharField(max_length=255, default="")
+    time = models.DateTimeField(auto_now_add = True)
