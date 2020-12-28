409b8c0ab66ecd3ce4ea3867810b7d47e7789769
Thanh Dang <s1f101910016@iniad.org>
2020-12-28 22:47:42 +0900

    Make shadow gopiece

--- go/frontend/src/components/Game.js
+++ go/frontend/src/components/Game.js
@@ -8,7 +8,7 @@ import {
   FormHelperText,
   Grid,
 } from "@material-ui/core";
-import ultilities from '../../static/css/ultilities.module.css';
+import ultilities from "../../static/css/ultilities.module.css";
 
 const imagesPath = "../../static/images/";
 const positionConst = {
@@ -39,26 +39,93 @@ console.log(positionImage);
 const EMPTY = ".";
 const WHITE = "w";
 const BLACK = "b";
+const WHITE_GHOST = "wg"; // for hover effect
+const BLACK_GHOST = "bg"; // for hover effect
 
 function Square(props) {
+  const ghostClass = ((props.value == BLACK_GHOST || props.value == WHITE_GHOST) ? ultilities["ghost"] : "");
   const goPiece = (
-          <svg height="23" width="23">
-          <defs>
-            <radialGradient id="blackPiece" cx="10%" cy="10%" r="70%" fx="20%" fy="20%">
-            <stop offset="0%" style={{"stop-color":"#C0C0C0"}} />
-            <stop offset="100%" style={{"stop-color":"black"}} />
-            </radialGradient>
-            <radialGradient id="whitePiece" cx="70%" cy="70%" r="95%" fx="20%" fy="20%">
-            <stop offset="0%" style={{"stop-color":"#FFFFFF"}} />
-            <stop offset="100%" style={{"stop-color":"#A9A9A9"}} />
-            </radialGradient>
-          </defs>
-          <circle cx="50%" cy="50%" r="45%" fill={`url(${props.value == BLACK ? "#blackPiece" : "#whitePiece"})`} />
-          </svg>);
-  const goSquare = ((props.value == EMPTY) ? null : goPiece);
-  
-  return <div onClick={() => props.onClick()} className={ultilities["imgOverlayWrap"]}><img src={props.srcImg}/>{goSquare}</div>;
-  // return (<div>{goPiece}<img src={props.srcImg} onClick={() => console.log("Clicked")} /></div>);
+    <svg height="23" width="23" className={ghostClass}>
+      <defs>
+        <radialGradient
+          id="blackPiece"
+          cx="10%"
+          cy="10%"
+          r="70%"
+          fx="20%"
+          fy="20%"
+        >
+          <stop offset="0%" style={{ "stop-color": "#C0C0C0" }} />
+          <stop offset="100%" style={{ "stop-color": "black" }} />
+        </radialGradient>
+        <radialGradient
+          id="whitePiece"
+          cx="70%"
+          cy="70%"
+          r="95%"
+          fx="20%"
+          fy="20%"
+        >
+          <stop offset="0%" style={{ "stop-color": "#FFFFFF" }} />
+          <stop offset="100%" style={{ "stop-color": "#A9A9A9" }} />
+        </radialGradient>
+      </defs>
+      <circle
+        cx="50%"
+        cy="50%"
+        r="45%"
+        fill={`url(${props.value.includes(BLACK) ? "#blackPiece" : "#whitePiece"})`}
+      />
+    </svg>
+  );
+  let goSquare = (props.value == EMPTY ? null : goPiece);
+  return (
+    <div
+      onMouseEnter={() => props.onMouseEnter()}
+      // onMouseLeave={() => props.onMouseLeave()}
+      onClick={() => props.onClick()}
+      className={ultilities["imgOverlayWrap"]}
+    >
+      <img src={props.srcImg} />
+      {goSquare}
+    </div>
+  );
+  // if (props.value == EMPTY) {
+  //   return (
+  //     <div
+  //       onMouseEnter={() => props.onMouseEnter()}
+  //       // onMouseLeave={() => props.onMouseLeave()}
+  //       onClick={() => props.onClick()}
+  //       className={ultilities["imgOverlayWrap"]}
+  //     >
+  //       <img src={props.srcImg} />
+  //     </div>
+  //   );
+  // } else if (props.value == BLACK_GHOST || props.value == WHITE_GHOST) {
+  //   return (
+  //     <div
+  //       onMouseEnter={() => props.onMouseEnter()}
+  //       // onMouseLeave={() => props.onMouseLeave()}
+  //       onClick={() => props.onClick()}
+  //       className={ultilities["imgOverlayWrap"] + " " + ultilities["ghost"]}
+  //     >
+  //       <img src={props.srcImg} className={ultilities["unghost"]}/>
+  //       {goSquare}
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div
+  //       onMouseEnter={() => props.onMouseEnter()}
+  //       // onMouseLeave={() => props.onMouseLeave()}
+  //       onClick={() => props.onClick()}
+  //       className={ultilities["imgOverlayWrap"]}
+  //     >
+  //       <img src={props.srcImg} />
+  //       {goSquare}
+  //     </div>
+  //   );
+  // }
 }
 
 class Board extends Component {
@@ -82,9 +149,11 @@ class Board extends Component {
         positionFlag |= jj != size - 1 ? positionConst.LEFT : 0;
         return (
           <Square
+            key={i * size + idx}
             value={value}
             srcImg={imagesPath + positionImage[String(positionFlag)]}
             onClick={() => this.props.onClick(i * size + idx)}
+            onMouseEnter={() => this.props.onMouseEnter(i * size + idx)}
           />
         );
       });
@@ -129,23 +198,46 @@ export default class Game extends Component {
   }
 
   onPlayerMove(idx) {
-    console.log("At " + idx);
+    
     const isWhite = this.state.isWhite;
     let boardArray = this.state.boardArray.slice();
-    if (boardArray[idx] == EMPTY) {
-      boardArray[idx] = (isWhite === true ? WHITE : BLACK);
+    if (boardArray[idx] == EMPTY || boardArray[idx] == WHITE_GHOST || boardArray[idx] == BLACK_GHOST) {
+      console.log("At " + idx);
+      boardArray[idx] = isWhite === true ? WHITE : BLACK;
       console.log(boardArray[idx]);
       // fetch api here to update board state
       this.setState({
-        isWhite: (isWhite == true ? false : true),
+        lastHover: null,
+        isWhite: isWhite == true ? false : true,
         boardArray: boardArray,
       });
     }
   }
 
-  leaveGameButtonPressed(){
+  onPlayerHover(idx) {
+    console.log("Hover at " + idx);
+    const isWhite = this.state.isWhite;
+    let boardArray = this.state.boardArray.slice();
+    if(this.state.lastHover != null)
+      boardArray[this.state.lastHover] = EMPTY;
+    if(boardArray[idx] == WHITE || boardArray[idx] == BLACK)
+      return;
+    if (boardArray[idx] == EMPTY) {
+      
+      boardArray[idx] = isWhite === true ? WHITE_GHOST : BLACK_GHOST;
+      console.log(boardArray[idx]);
+      // fetch api here to update board state
+    }
+    this.setState({
+      lastHover: idx,
+      isWhite: isWhite,
+      boardArray: boardArray,
+    });
+  }
+
+  leaveGameButtonPressed() {
     this.props.leaveGameCallback();
-    this.props.history.push('/');
+    this.props.history.push("/");
   }
 
   render() {
@@ -156,17 +248,20 @@ export default class Game extends Component {
         : `(Black): ${this.state.blackPlayer}`);
     return (
       <div>
-        <button onClick={() => this.leaveGameButtonPressed()}>Leave game</button>
+        <button onClick={() => this.leaveGameButtonPressed()}>
+          Leave game
+        </button>
         <div>{crntPlayer}</div>
         <Grid container spacing={4}>
           <Grid item xs={8}>
             <Board
               boardArray={this.state.boardArray}
               onClick={(idx) => this.onPlayerMove(idx)}
+              onMouseEnter={(idx) => this.onPlayerHover(idx)}
             />
           </Grid>
           <Grid item xs={4}>
-            <Chat chatLog={this.state.chatLog}/>
+            <Chat chatLog={this.state.chatLog} />
           </Grid>
         </Grid>
       </div>
